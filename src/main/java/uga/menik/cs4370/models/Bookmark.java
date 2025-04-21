66a428e4a0e766bba0a49f59dffd18d51a697fff
Grace <ger85847@uga.edu>
2025-04-21 10:21:42 -0400

    userservice

---
+++ src/main/java/uga/menik/cs4370/models/Bookmark.java
@@ -0,0 +1,5 @@
+package uga.menik.cs4370.models;
+
+public class Bookmark {
+    
+}
---
+++ src/main/java/uga/menik/cs4370/services/BookmarkService.java
@@ -0,0 +1,8 @@
+package uga.menik.cs4370.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookmarkService {
+    
+}
